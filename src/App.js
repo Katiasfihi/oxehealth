d14f6cd5150cdd674e91ae166ab96cd147882697
Katia <katia.sfihi@gmail.com>
2021-05-10 11:53:51 +0100

    posting to local json server wip + onChange / onSubmit wip

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.scss';
 function App() {
 
   const [data, setData ] = useState([])
-  const [send, setSend ] = useState('')
   const [isOpen, setIsOpen] = useState(false);
+  const url = ''
+  const [postId, setPostId] = useState(null)
+  const [input, setInput] = useState({name: ''})
  
   const togglePopup = () => {
     setIsOpen(!isOpen);
@@ -18,13 +20,40 @@ function App() {
     setData(data);
  } 
 
-  useEffect((e) => {
+
+  function sendMyData () {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: 'React Hooks POST Request Example' })
+  };
+  fetch('http://localhost:3000/rovers', requestOptions)
+  .then (response => response.json())
+  .then (data=> setPostId(data.name));
+  }
+ 
+  useEffect(() => {
     getMyData()
-  }, [] );
+    sendMyData()
+  }, []);
+
+function submit(e){
+  e.preventDefault();
+  sendMyData()
+}
+
+function handle(e){
+  const newInput = {...input}
+  newInput[e.target.id] = e.target.value
+  setInput(newInput)
+  console.log(newInput)
+}
+  
 
   return (
     <div className="dashboard">
       <header className='dashboard__header'>Mars Rovers</header>
+      <div>hello{postId}</div>
           <div className='dashboard__content'>
           {
               data.map((el)=>
@@ -46,17 +75,17 @@ function App() {
               content={<>
                 <h1 className='h1'>New Rover</h1>
                 <p className='h2'>Launch new rover to mars</p>
-                <input  placeholder='Rover Name' className='input-rover-name'></input>
+                <input  placeholder='Rover Name' className='input-rover-name' onChange={(e)=>handle(e)}></input>
                 <div className='popup-button'>
                 <button className='popup-button__one' onClick={togglePopup}>Cancel</button>
-                <button className='popup-button__one'>Launch</button>
-                </div>
-                
+                <form onSubmit={(e) => submit(e)}>
+                <button className='popup-button__one' >Launch</button>
+                </form>              
+                </div>  
               </>}
               handleClose={togglePopup}
             />}
-          </footer>
-         
+          </footer>         
     </div>
   );
 }
